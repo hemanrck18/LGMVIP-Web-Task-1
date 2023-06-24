cce7bd8c7e87fb64c42f833e6405c7b09f562233
hemanrck18 <114051971+hemanrck18@users.noreply.github.com>
2023-06-24 19:21:07 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+const DATA = [
+  { id: "todo-0", name: "Task-1", completed: false },
+  { id: "todo-1", name: "Task-2", completed: false },
+  { id: "todo-2", name: "Task-3", completed: false },
+];
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App tasks={DATA} />
+  </React.StrictMode>
+);
